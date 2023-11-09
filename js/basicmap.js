8060cdfbfd0d5c25a65af93f9b84bc7c707a08dc
Allen Domalik <allenDomalik@yahoo.com>
2023-11-08 20:14:33 -0600

    Update basicmap.js

--- js/basicmap.js
+++ js/basicmap.js
@@ -4,7 +4,7 @@ function init(){
 	var myLocation = new google.maps.LatLng(18.402990410469542, -88.39656847253687);
 	var mapOptions = {
 		center: myLocation,
-		zoom: 18,
+		zoom: 20,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
 		mapTypeControlOptions: {
 			position: google.maps.ControlPosition.BOTTOM_CENTER
@@ -20,7 +20,7 @@ function init(){
 		icon: 'images/Toucan_Belize.jpg'
 	});
 
-	var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
+	var contentString = '<h1>Belize Corozal</h1><p>Corozal Town is a town in Belize, capital of Corozal District. Corozal Town is located about 84 miles north of Belize City, and 9 miles from the border with Mexico. The population of Corozal Town, according to the main results of the 2010 census, is 9,871</p>';
 
 	var infowindow = new google.maps.InfoWindow({
       content: contentString
