05fbdf34a3b1b6d66ac5ddfcb4dbd4b32fe04631
Allen Domalik <allenDomalik@yahoo.com>
2023-11-08 19:58:39 -0600

    Update basicmap.js

--- js/basicmap.js
+++ js/basicmap.js
@@ -1,7 +1,7 @@
 function init(){
 	//alert('it works');
 	var el = document.getElementById('frame');
-	var myLocation = new google.maps.LatLng(182338.4, 882231.9);
+	var myLocation = new google.maps.LatLng(182338.4);
 	var mapOptions = {
 		center: myLocation,
 		zoom: 18,
